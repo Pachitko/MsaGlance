0ded68088976978dae51d2da346fa61916952b36
Pachitko <pachitko@mail.ru>
2022-06-10 11:29:41 +0300

    Add integration settings to Web.Api service

---
+++ src/Services/Web/Web.Api/Options/IntegrationOptions.cs
@@ -0,0 +1,10 @@
+namespace Web.Api.Options;
+
+public class IntegrationOptions
+{
+#nullable disable
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string DiskApiEndpoint { get; set; }
+#nullable enable
+}
--- src/Services/Web/Web.Api/Program.cs
+++ src/Services/Web/Web.Api/Program.cs
@@ -4,13 +4,18 @@ using System.Threading.Tasks;
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Serilog;
+using Web.Api.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
+
+builder.Services.Configure<IntegrationOptions>(builder.Configuration.GetRequiredSection("Integration"));
 
 builder.Host.UseSerilog();
 Log.Logger = new LoggerConfiguration()
@@ -30,7 +35,7 @@ app.Map("/files", GetFilesAsync);
 
 app.Run();
 
-static async Task<IResult> GetFilesAsync(IHttpClientFactory httpClientFactory, ILogger<Program> logger)
+static async Task<IResult> GetFilesAsync(IHttpClientFactory httpClientFactory, ILogger<Program> logger, IOptions<IntegrationOptions> integration)
 {
     var authClient = httpClientFactory.CreateClient();
     DiscoveryDocumentRequest discoveryDocumentRequest = new()
@@ -46,14 +51,15 @@ static async Task<IResult> GetFilesAsync(IHttpClientFactory httpClientFactory, I
     var tokenResponse = await authClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
     {
         Address = discoveryDocument.TokenEndpoint,
-        ClientId = "client_id",
-        ClientSecret = "client_secret",
-        Scope = "DiskApi"
+        ClientId = integration.Value.ClientId,
+        ClientSecret = integration.Value.ClientSecret,
+        Scope = "disk.api.read"
     });
 
     var diskClient = httpClientFactory.CreateClient();
+    diskClient.BaseAddress = new Uri(integration.Value.DiskApiEndpoint);
     diskClient.SetBearerToken(tokenResponse.AccessToken);
-    var response = await diskClient.GetAsync("http://disk/secret");
+    var response = await diskClient.GetAsync("/secret");
 
     if (response.IsSuccessStatusCode)
     {
